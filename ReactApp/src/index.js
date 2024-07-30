c88b21b84da505506a266a972ad8f4bc00367392
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-07-30 17:47:07 +0700

    Create React App

---
+++ ReactApp/src/index.js
@@ -0,0 +1,15 @@
+// Import library
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create App Component
+function App() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+// Render App Component to the root element
+ReactDOM.render(<App />, document.getElementById("root"));
