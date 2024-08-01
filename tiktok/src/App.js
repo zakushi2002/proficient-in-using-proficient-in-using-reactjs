4f4c1f0ee0cdb34e7ed8c0050bdb0a927cddb9d2
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-01 11:45:59 +0700

    basic useState hooks

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const products = [18, 8, 2002];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(() => {
+        const total = products.reduce((total, product) => total + product);
+        console.log(total);
+        return total;
+    });
+
+    const handleIncrement = () => {
+        setCounter(counter + 1);
+    };
+    const [myInfo, setMyInfo] = useState({
+        name: "Toàn Huỳnh Thanh Nguyễn",
+        age: 22,
+        email: "toannguyenit239@gmail.com",
+    });
+
+    const handleUpdateInfo = () => {
+        setMyInfo((preData) => {
+            return {
+                ...preData,
+                major: "Software Engineering",
+            };
+        });
+    };
+    return (
+        <div className="App" style={{ padding: 20 }}>
+            <h1>{counter}</h1>
+            <button onClick={handleIncrement}>Increment</button>
+
+            <h2>Name: {myInfo.name}</h2>
+            <h2>Age: {myInfo.age}</h2>
+            <h2>Email: {myInfo.email}</h2>
+            <h2>Major: {myInfo.major}</h2>
+            <button onClick={handleUpdateInfo}>Update Info</button>
+        </div>
+    );
 }
 
 export default App;
