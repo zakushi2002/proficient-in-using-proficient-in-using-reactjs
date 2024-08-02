f846860680198479bbffe05c219553a32fa5f675
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-02 12:35:32 +0700

    Two-way binding input

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,26 +1,17 @@
 import { useState } from "react";
-const gifts = [
-    {
-        name: "Màn hình Monitor Xiaomi 27 inch A27i ELA5345EU",
-    },
-    {
-        name: "Xiaomi POCO X6 Pro 5G 8GB 256GB",
-    },
-    {
-        name: "Tai nghe Bluetooth Apple AirPods Pro 2 2023 USB-C",
-    },
-];
+
 function App() {
-    const contentNoGift = "Click to get a gift";
-    const [gift, setGift] = useState(contentNoGift);
-    const randomGift = () => {
-        const randomIndex = Math.floor(Math.random() * gifts.length);
-        setGift(gifts[randomIndex].name);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const handleSignUp = () => {
+        // Sign up logic goes here
+        console.log("Form sign up: ", { name, email });
     };
     return (
         <div className="App" style={{ padding: 32 }}>
-            <h1>{gift}</h1>
-            <button onClick={randomGift}>Get</button>
+            <input onChange={(e) => setName(e.target.value)} value={name} />
+            <input onChange={(e) => setEmail(e.target.value)} value={email} />
+            <button onClick={handleSignUp}>Sign up</button>
         </div>
     );
 }
