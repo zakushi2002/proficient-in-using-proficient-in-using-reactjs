220f80ff27a57cd819d5198ee5e517913c1a225e
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-04 13:46:40 +0700

    useEffect with timer functions

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -13,82 +13,23 @@ import { useEffect, useState } from "react";
 // a. Callback luôn được gọi sau khi component mounted
 // b. Cleanup function luôn được gọi trước khi component unmounted
 
-const tabs = ["todos", "albums", "posts"];
-
 function Content() {
-    const [title, setTitle] = useState("");
-    const [tasks, setTasks] = useState([]);
-    const [kind, setKind] = useState(tabs[0]);
-    const [scrollUpButton, setScrollUpButton] = useState(false);
-    const [width, setWidth] = useState(window.innerWidth);
+    const [countDown, setCountDown] = useState(120);
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${kind}`)
-            .then((response) => response.json())
-            .then((tasks) => setTasks(tasks));
-    }, [kind]);
+        const timerId = setInterval(() => {
+            setCountDown((prev) => prev - 1);
+        }, 1000);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrollUpButton(window.scrollY >= 200);
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        console.log("Mounted...");
         return () => {
-            window.removeEventListener("scroll", handleScroll);
-            console.log("Cleanup...");
+            clearInterval(timerId);
         };
     }, []);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWidth(window.innerWidth);
-        };
-
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("scroll", handleResize);
-        };
-    }, []);
     return (
         <div>
-            <h1>{width}</h1>
-            {tabs.map((tab) => (
-                <button
-                    key={tab}
-                    style={
-                        kind === tab
-                            ? { color: "#fff", backgroundColor: "#333" }
-                            : {}
-                    }
-                    onClick={() => setKind(tab)}
-                >
-                    {tab}
-                </button>
-            ))}
-            <input value={title} onChange={(e) => setTitle(e.target.value)} />
-            <ul>
-                {tasks.map((task) => (
-                    <li key={task.id}>{task.title}</li>
-                ))}
-            </ul>
-            {scrollUpButton && (
-                <button
-                    onClick={() => {
-                        window.scrollTo({ top: 0, behavior: "smooth" });
-                    }}
-                    style={{
-                        position: "fixed",
-                        bottom: 16,
-                        right: 16,
-                        padding: 8,
-                        borderRadius: 8,
-                    }}
-                >
-                    Scroll up
-                </button>
-            )}
+            <h1>Count down</h1>
+            <h2>{countDown}</h2>
         </div>
     );
 }
