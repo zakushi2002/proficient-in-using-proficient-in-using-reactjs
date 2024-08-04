18a30bb207353aaa3d1c3c7b77fe9532d5135256
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-04 16:14:44 +0700

    useRef hook

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,38 +1,40 @@
-import { useEffect, useState, useLayoutEffect } from "react";
-
-// useEffect
-// 1. Cập nhật lại state
-// 2. Cập nhật DOM (mutated)
-// 3. Render lại UI
-// 4. Gọi clean-up nếu dependencies thay đổi
-// 5. Gọi useEffect callback
-
-// useLayoutEffect
-// 1. Cập nhật lại state
-// 2. Cập nhật DOM (mutated)
-// 3. Gọi clean-up nếu dependencies thay đổi (sync) *
-// 4. Gọi useLayoutEffect callback (sync) *
-// 5. Render lại UI
+import { useRef, useState, useEffect } from "react";
 
 function Content() {
-    const [count, setCount] = useState(0);
-    useLayoutEffect(() => {
-        if (count > 3) {
-            setCount(0);
-        }
-        return () => {
-            console.log("Clean-up");
-        };
+    const [count, setCount] = useState(60);
+    let timerId = useRef();
+    const preCount = useRef();
+    const h1Ref = useRef();
+
+    useEffect(() => {
+        preCount.current = count;
     }, [count]);
 
-    const handleRun = () => {
-        setCount(count + 1);
+    const handleStart = () => {
+        timerId.current = setInterval(() => {
+            setCount((preData) => preData - 1);
+        }, 1000);
+        console.log(`====> Start Timer ${timerId.current}`);
     };
+    const handleStop = () => {
+        clearInterval(timerId.current);
+        console.log(`====> Stop Timer ${timerId.current}`);
+    };
+
+    console.log(
+        `====> Render: count - ${count} & preCount - ${preCount.current}`
+    );
+
+    useEffect(() => {
+        const rect = h1Ref.current.getBoundingClientRect();
+        console.log(rect);
+    }, [count]);
 
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={handleRun}>Run</button>
+            <h1 ref={h1Ref}>{count}</h1>
+            <button onClick={handleStart}>Run</button>
+            <button onClick={handleStop}>Stop</button>
         </div>
     );
 }
