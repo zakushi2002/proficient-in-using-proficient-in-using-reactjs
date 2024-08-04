966d30e0548ce8643ce95bf5ad166690ac252dd9
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-04 14:48:52 +0700

    useEffect with fake Chat App

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -13,42 +13,39 @@ import { useEffect, useState } from "react";
 // a. Callback luôn được gọi sau khi component mounted
 // b. Cleanup function luôn được gọi trước khi component unmounted
 // c. Cleanup function luôn được gọi trước khi callback được gọi lần tiếp theo (trừ lần component mounted)
-
+const rooms = [
+    { id: 1, name: "ELMS" },
+    { id: 2, name: "Finance" },
+    { id: 3, name: "Family Circle" },
+];
 function Content() {
-    const [count, setCount] = useState(1);
-    const [avatar, setAvatar] = useState();
+    const [roomId, setRoomId] = useState(1);
 
     useEffect(() => {
-        console.log(
-            `Mounted or re-rendered ${count} ${count === 1 ? "time" : "times"}`
-        );
-
-        return () => {
-            console.log(
-                `Cleaned up ${count} ${count === 1 ? "time" : "times"}`
-            );
+        const handlerMessage = ({ detail }) => {
+            console.log(detail.message);
         };
-    }, [count]);
-
-    const handlePreviewAvatar = (e) => {
-        const file = e.target.files[0];
-        file.preview = URL.createObjectURL(file);
-        setAvatar(file);
-    };
-
-    useEffect(() => {
+        window.addEventListener(`room-${roomId}`, handlerMessage);
         return () => {
-            avatar && URL.revokeObjectURL(avatar.preview);
+            window.removeEventListener(`room-${roomId}`, handlerMessage);
         };
-    }, [avatar]);
+    }, [roomId]);
 
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>Increase</button>
-            <input type="file" onChange={handlePreviewAvatar} />
-            <br />
-            {avatar && <img src={avatar.preview} alt="avatar" width="60%" />}
+            <ul>
+                {rooms.map((room) => (
+                    <li
+                        key={room.id}
+                        style={{
+                            color: room.id === roomId ? "red" : "black",
+                        }}
+                        onClick={() => setRoomId(room.id)}
+                    >
+                        {room.name}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
--- tiktok/src/index.js
+++ tiktok/src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Fake message
+function emitMessage(id) {
+    setInterval(() => {
+        window.dispatchEvent(
+            new CustomEvent(`room-${id}`, {
+                detail: { message: `Hello room ${id}` },
+            })
+        );
+    }, 2500);
+}
+
+emitMessage(1);
+emitMessage(2);
+emitMessage(3);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
