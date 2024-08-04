61dd896bf2d6f3270d2e75dc087b20bd4fecde0d
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-04 21:52:17 +0700

    useMemo hook

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,24 +1,60 @@
-import { useCallback, useState } from "react";
-import Content from "./Content";
+import { useState, useMemo, useRef } from "react";
 
-// 1. memo() => Higher Order Component (HOC)
-// 2. useCallBack()
-// - Reference types
-// - React.memo()
-
-// Hooks
-// HOC
-// Render Props
+// useMemo is used to avoid re-executing any logic unnecessarily.
 
 function App() {
-    const [count, setCount] = useState(0);
-    const handleIncrease = useCallback(() => {
-        setCount((preData) => preData + 1);
-    }, []);
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+    const [products, setProducts] = useState([]);
+    const inputNameRef = useRef();
+
+    const handleSubmit = () => {
+        setProducts((prevProducts) => [
+            ...prevProducts,
+            {
+                name: name,
+                price: +price,
+            },
+        ]);
+        setName("");
+        setPrice("");
+        inputNameRef.current.focus();
+    };
+
+    const totalPrice = useMemo(() => {
+        const result = products.reduce((acc, product) => {
+            return acc + product.price;
+        }, 0);
+        console.log("Calculating...");
+        return result;
+    }, [products]);
+
     return (
         <div className="App" style={{ padding: `10px 32px` }}>
-            <Content onIncrease={handleIncrease} />
-            <h1>{count}</h1>
+            <input
+                ref={inputNameRef}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter product name..."
+            />
+            <br />
+            <input
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                placeholder="Enter product price..."
+            />
+            <br />
+            <button onClick={handleSubmit}>Add Product</button>
+            <br />
+            Total Price: {totalPrice}
+            <br />
+            <ul>
+                {products.map((product, index) => (
+                    <li key={index}>
+                        {product.name} - {product.price}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
