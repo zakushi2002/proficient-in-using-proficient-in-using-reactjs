369baab72609262512d233ceff4c4954c730aadf
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-04 22:11:42 +0700

    useReducer hook

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,60 +1,43 @@
-import { useState, useMemo, useRef } from "react";
+import { useState, useReducer } from "react";
 
-// useMemo is used to avoid re-executing any logic unnecessarily.
+// useState
+// 1. Initial state: 0
+// 2. Action: Increase (state +1) or Decrease (state -1)
+
+// useReducer
+// 1. Initial state: 0
+// 2. Action: Increase (state +1) or Decrease (state -1)
+// 3. Reducer: (state, action) => state
+// 4. Dispatch: dispatch(action)
+
+// Initial state
+const initialState = 0;
+
+// Actions
+const INCREASE_ACTION = "INCREASE";
+const DECREASE_ACTION = "DECREASE";
+
+// Reducer
+const reducer = (state, action) => {
+    console.log("Reducer", state, action);
+    switch (action) {
+        case INCREASE_ACTION:
+            return state + 1;
+        case DECREASE_ACTION:
+            return state - 1;
+        default:
+            throw new Error(`Unsupported action type: ${action}`);
+    }
+};
 
 function App() {
-    const [name, setName] = useState("");
-    const [price, setPrice] = useState("");
-    const [products, setProducts] = useState([]);
-    const inputNameRef = useRef();
-
-    const handleSubmit = () => {
-        setProducts((prevProducts) => [
-            ...prevProducts,
-            {
-                name: name,
-                price: +price,
-            },
-        ]);
-        setName("");
-        setPrice("");
-        inputNameRef.current.focus();
-    };
-
-    const totalPrice = useMemo(() => {
-        const result = products.reduce((acc, product) => {
-            return acc + product.price;
-        }, 0);
-        console.log("Calculating...");
-        return result;
-    }, [products]);
+    const [count, dispatch] = useReducer(reducer, initialState);
 
     return (
         <div className="App" style={{ padding: `10px 32px` }}>
-            <input
-                ref={inputNameRef}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Enter product name..."
-            />
-            <br />
-            <input
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-                placeholder="Enter product price..."
-            />
-            <br />
-            <button onClick={handleSubmit}>Add Product</button>
-            <br />
-            Total Price: {totalPrice}
-            <br />
-            <ul>
-                {products.map((product, index) => (
-                    <li key={index}>
-                        {product.name} - {product.price}
-                    </li>
-                ))}
-            </ul>
+            <h1>Count: {count}</h1>
+            <button onClick={() => dispatch(INCREASE_ACTION)}>Increase</button>
+            <button onClick={() => dispatch(DECREASE_ACTION)}>Decrease</button>
         </div>
     );
 }
