e180ddb048c5761c3547c27a76018267d4c3a48d
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-04 13:34:53 +0700

    useEffect with DOM events

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -11,6 +11,7 @@ import { useEffect, useState } from "react";
 // ==============================
 // General characteristics
 // a. Callback luôn được gọi sau khi component mounted
+// b. Cleanup function luôn được gọi trước khi component unmounted
 
 const tabs = ["todos", "albums", "posts"];
 
@@ -18,14 +19,41 @@ function Content() {
     const [title, setTitle] = useState("");
     const [tasks, setTasks] = useState([]);
     const [kind, setKind] = useState(tabs[0]);
+    const [scrollUpButton, setScrollUpButton] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${kind}`)
             .then((response) => response.json())
             .then((tasks) => setTasks(tasks));
     }, [kind]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollUpButton(window.scrollY >= 200);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        console.log("Mounted...");
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+            console.log("Cleanup...");
+        };
+    }, []);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("scroll", handleResize);
+        };
+    }, []);
     return (
         <div>
+            <h1>{width}</h1>
             {tabs.map((tab) => (
                 <button
                     key={tab}
@@ -45,6 +73,22 @@ function Content() {
                     <li key={task.id}>{task.title}</li>
                 ))}
             </ul>
+            {scrollUpButton && (
+                <button
+                    onClick={() => {
+                        window.scrollTo({ top: 0, behavior: "smooth" });
+                    }}
+                    style={{
+                        position: "fixed",
+                        bottom: 16,
+                        right: 16,
+                        padding: 8,
+                        borderRadius: 8,
+                    }}
+                >
+                    Scroll up
+                </button>
+            )}
         </div>
     );
 }
