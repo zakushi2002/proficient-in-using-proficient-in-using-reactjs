3203239cc0db70a14403cfacbd517a4cdd5bda4c
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-04 16:32:04 +0700

    React.memo HOC

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import Content from "./Content";
 
+// 1. memo() => Higher Order Component (HOC)
+// 2. useCallBack()
+
+// Hooks
+// HOC
+// Render Props
+
 function App() {
-    const [show, setShow] = useState(false);
-    const showContent = () => setShow(!show);
+    const [count, setCount] = useState(0);
+    const increase = () => {
+        setCount((preData) => preData + 1);
+    };
     return (
-        <div className="App" style={{ padding: 32 }}>
-            <button onClick={showContent}>Toggle</button>
-            {show && <Content />}
+        <div className="App" style={{ padding: `10px 32px` }}>
+            <Content count={count} />
+            <h1>{count}</h1>
+            <button onClick={increase}>Increase</button>
         </div>
     );
 }
--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,42 +1,11 @@
-import { useRef, useState, useEffect } from "react";
-
-function Content() {
-    const [count, setCount] = useState(60);
-    let timerId = useRef();
-    const preCount = useRef();
-    const h1Ref = useRef();
-
-    useEffect(() => {
-        preCount.current = count;
-    }, [count]);
-
-    const handleStart = () => {
-        timerId.current = setInterval(() => {
-            setCount((preData) => preData - 1);
-        }, 1000);
-        console.log(`====> Start Timer ${timerId.current}`);
-    };
-    const handleStop = () => {
-        clearInterval(timerId.current);
-        console.log(`====> Stop Timer ${timerId.current}`);
-    };
-
-    console.log(
-        `====> Render: count - ${count} & preCount - ${preCount.current}`
-    );
-
-    useEffect(() => {
-        const rect = h1Ref.current.getBoundingClientRect();
-        console.log(rect);
-    }, [count]);
-
+import { memo } from "react";
+function Content({ count }) {
+    console.log("Content");
     return (
         <div>
-            <h1 ref={h1Ref}>{count}</h1>
-            <button onClick={handleStart}>Run</button>
-            <button onClick={handleStop}>Stop</button>
+            <h1>HOC {count}</h1>
         </div>
     );
 }
 
-export default Content;
+export default memo(Content);
