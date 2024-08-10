f093fa8dcac5383b1d45d452b7cfc1d92be85dd3
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-10 18:59:35 +0700

    clsx & classnames

---
+++ tiktok/src/components/Button/index.js
@@ -0,0 +1,13 @@
+import clsx from "clsx";
+import styles from "./Button.module.css";
+
+function Button({ primary }) {
+    const classes = clsx(styles.btn, {
+        [styles.primary]: primary,
+        "d-flex": true,
+    });
+
+    return <button className={classes}>Click me!</button>;
+}
+
+export default Button;
