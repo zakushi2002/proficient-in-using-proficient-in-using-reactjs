1d2a2d5f3402b18f1cbe4eda3089795e37149f78
Nguyễn Huỳnh Thanh Toàn <toannguyenit239@gmail.com>
2024-08-19 11:30:24 +0700

    Build default Layout frame

--- tiktok-ui/src/components/Layout/HeaderOnly/index.js
+++ tiktok-ui/src/components/Layout/HeaderOnly/index.js
@@ -4,9 +4,7 @@ function HeaderOnly({ children }) {
     return (
         <div>
             <Header />
-            <div className="container">
-                <div className="content">{children}</div>
-            </div>
+            <div className="content">{children}</div>
         </div>
     );
 }
